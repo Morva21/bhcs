c6823c4da29ff9160abcfc6eecae95ae6d6204bb
Derescio <ddw.web.dev.services@gmail.com>
2023-11-14 23:18:44 -0500

    set up backend server

--- api/index.js
+++ api/index.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
 app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
