9abbce3b05df2875ae52157a9f8ca04d79e8c71d
Derescio <ddw.web.dev.services@gmail.com>
2023-11-18 21:27:38 -0500

    Updated build configurations.

--- api/index.js
+++ api/index.js
@@ -3,12 +3,19 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import emailRoute from './routes/emailRoutes.js'
+import path from 'path'
 dotenv.config()
 
-
+const __dirname =path.resolve()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+app.use(express.static(path.join(__dirname , '/client/dist')))
+app.get('*', (req, res)=>{
+res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+
+})
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
