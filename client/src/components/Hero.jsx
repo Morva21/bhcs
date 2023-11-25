0039669ac7ed56a83d140659a77c1230fbd4efca
Derescio <ddw.web.dev.services@gmail.com>
2023-11-24 20:06:29 -0500

    Updated Hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <section id='hero' className="hero py-1 bg-grey rounded-lg  xl:pb-0 overflow-hidden">
+      <section id='hero' className="hero bg-grey py-12 xl:pt-12 rounded-lg  xl:pb-0 overflow-hidden">
         <div className="container mx-auto h-ful">
             <div className="md:grid md:grid-cols-2 md:gap-8">
               {/* <!-- Left Column --> */}
               <div className="md:order-1">
-                <div className="flex items-center  bg-white py-[10px] px-[20px] w-max gap-x-2 mb-[26px] rounded-full mx-auto xl:mx-0">
+                <div className="flex items-center mt-2 bg-white py-[10px] px-[20px] w-max gap-x-2 mb-[26px] rounded-full mx-auto xl:mx-0">
                     <i className="fa-solid fa-heart-pulse text-accent"></i>
                     <div className="uppercase text-base text-xs font-medium tracking-[2.24px]"> we can help</div>
                 </div>
