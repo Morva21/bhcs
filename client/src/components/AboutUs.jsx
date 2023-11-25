c9f0cf60049870cd57c8708ba8d8bf956cba7826
Derescio <ddw.web.dev.services@gmail.com>
2023-11-24 22:17:30 -0500

    Last Update

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -4,44 +4,28 @@ const AboutUs = () => {
   return (
     <section id= 'about-us' className="   rounded-[15px] xl:pb-0 overflow-hidden"> 
     <div className='container mx-auto h-full font-semibold'>
-     {/* <div className='md:grid md:grid-cols-2 md:gap-8'>
-     <div className="md:order-1">
-              
-                <div >
-                    <h1 className="h1 mb-6 md:mr-40 text-accent">About Us.</h1>
-                    <img src="/img/team/Picture1.jpg"  className=' rounded-lg shadow-xl'  alt="" />
-                </div>
-                
-              </div>
-              <div className="md:order-2 mt-3">
-                <p>
-                At Bernadette Home Care Services, we believe every life is precious and deserving of utmost care and compassion.  As a dedicated care provide, we are committed to enhancing the lives our our clients and have been doing so for many years.  
-
-Our Mission is to provide unparalleled support and comfort to every one, ensuring they lead fulfilling lives in the comfort of their homes.  We will provide a customized plan that will change as your loved one need change.  We provide short and long term, overnight and twenty-four hour care.  We will travel to all areas to meet your needs and we are committed 100% to our clients.  With a team of highly trained and compassionate caregivers, we offer a warm and nurturing care where everyone will feel valued, respected and cherished. 
-                </p>
-                <div className='mt-6'>
-                  <h2 className='h2 text-accent'>Our Values</h2>
-                  <p>
-We live by our core values and tirelessly promote them at all levels. Responding at a moment's notice. Integrity without compromise. 
-Celebrating an improved quality of life. Fostering a positive attribute.</p>
-                </div>
-              </div>
-     </div> */}
-     <h2 className='h2 text-accent'>About Us</h2>
-     <p>At Bernadette Home Care Services, we believe every life is precious and deserving of 
+      <h2 className='h2 text-accent'>About Us</h2>
+     <p className='mb-12'>At Bernadette Home Care Services, we believe every life is precious and deserving of 
       utmost care and compassion.  As a dedicated care provider, 
       we are committed to enhancing the lives of our clients and 
       have been doing so for many years.  
 
 Our Mission is to provide unparalleled support and comfort to every one, ensuring they lead fulfilling lives in the comfort of their homes.  We will provide a customized plan that will change as your loved one needs change.  We provide short and long term, overnight and twenty-four hour care.  We will travel to all areas to meet your needs and we are committed 100% to our clients.  With a team of highly trained and compassionate caregivers, we offer a warm and nurturing care where everyone will feel valued, respected and cherished. </p>
-     <h2 className='h2 text-accent'>Our Values</h2>
-     <div className="h5 font-semibold">
-      <h6>We live by our core values and tirelessly promote them at all levels</h6>
-      <h2>Responding at a moment's notice</h2>
-      <h2>Integrity without compromise</h2>
-      <h2>Celebrating an improved quality of life</h2>
-      <h2>Fostering a positive attitude</h2>
-     </div>
+      
+
+  
+
+     
+ <h2 className='h2 text-accent mb-2'>Our Values</h2>
+  
+     
+  <div className="h5 font-semibold mb-2">
+   <h6>We live by our core values and tirelessly promote them at all levels</h6>
+   <h2>Responding at a moment's notice</h2>
+   <h2>Integrity without compromise</h2>
+   <h2>Celebrating an improved quality of life</h2>
+   <h2>Fostering a positive attitude</h2>
+  </div>
     </div>
     </section>
   )
